ed4d078fd25ab0549765a00ba8be47414d52c38f
JinnyHui <hui@iupui.edu>
2018-09-13 04:50:26 -0400

    finished
    bug free and tested with test cases

--- JH_myKmeans.py
+++ JH_myKmeans.py
@@ -6,6 +6,7 @@ import sys
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 plt.style.use('ggplot')
 
 
@@ -53,8 +54,8 @@ def SSE(cluster, dataset):
     """
 
     SSE_cluster = 0
-    for point in cluster.pointAssign:
-        SSE_cluster += np.linalg.norm(cluster.centroid, dataset[point - 1])**2
+    for point in cluster.pointStore:
+        SSE_cluster += np.linalg.norm(cluster.centroid-dataset[point - 1])**2
     return SSE_cluster
 
 
@@ -65,32 +66,39 @@ def mean(cluster, dataset):
     :param dataset: the whole dataset
     :return: the mean of the cluster
     """
-    index = np.array(cluster.pointAssign) - 1
-    cluster_array = dataset[index]
-    cluster_mean = np.mean(cluster_array, axis=0)
-    return cluster_mean
+    if not cluster.pointAssign:
+        return 0
+    else:
+        index = np.array(cluster.pointAssign) - 1
+        cluster_array = dataset[index]
+        cluster_mean = np.mean(cluster_array, axis=0)
+        return cluster_mean
 
 
 class Cluster(object):
     def __init__(self):
         self.centroid = None
         self.pointAssign = []  # a list of integers representing each line number in the dataset
+        self.pointStore = []
         self.pre_mean = None
         self.mean = None
         self.SSE = 0
 
     def display(self):
         print('Mean: ' + str(self.mean))
-        print('Cluster assignment: ' + str(self.pointAssign))
-        print('Size of cluster: ' + str(len(self.pointAssign)))
+        print('Cluster assignment: ' + str(self.pointStore))
+        print('Size of cluster: ' + str(len(self.pointStore)))
+        print('')
 
 
 ##
-dataFile = sys.args[0]  # get the name of the input file
-clusterNumber = int(sys.args[1])  # get the number of clusters
-centroidFile = sys.args[2]  # get the name of the file listing centroids if any
-dataPointsRaw = np.genfromtxt(dataFile, delimiter=',', dtype=None)  # ndarray with label
-dataPoints = dataPointsRaw[:][:-2]  # dataset w/o label
+dataFile = sys.argv[1]  # get the name of the input file
+clusterNumber = int(sys.argv[2])  # get the number of clusters
+centroidFile = None
+if len(sys.argv) > 3:
+    centroidFile = sys.argv[3]  # get the name of the file listing centroids if any
+dataPointsRaw = np.array(pd.read_csv(dataFile, sep=",", header=None))  # ndarray with label
+dataPoints = dataPointsRaw[:, :-1]  # dataset w/o label
 pointNumber, dimension = dataPoints.shape  # get the shape of the dataset
 SSE_sum = 0  # the sum of SSE of each cluster
 epsilon = 0.0001
@@ -109,41 +117,54 @@ else:
 for i in centroids:
     c = Cluster()
     c.centroid = dataPoints[int(i)-1]
-    clusterList.append()
+    clusterList.append(c)
+# print(centroids)
 
 # iterations of k-means algorithm; terminate when change <= epsilon
 while change > epsilon:
     iteration += 1
     change = 0
     for i in range(len(dataPoints)):  # assign all the points to clusters
-        dist_min = float('-inf')
-        cluster_index = -1
+        dist_min = float('inf')
+        cluster_index = None
         for j in range(clusterNumber):
             a = dataPoints[i]
             b = clusterList[j].centroid
-            if np.linalg.norm(a, b) < dist_min:
+            dist = np.linalg.norm(a-b)
+            if dist < dist_min:
+                dist_min = dist
                 cluster_index = j
         clusterList[cluster_index].pointAssign.append(i+1)
 
-    for i in range(clusterNumber):  # calculate the mean and assign it as the new centroid
-        clusterList[i].pre_mean = clusterList[i].centroid
-        clusterList[i].mean = mean(clusterList[i], dataPoints)
-        clusterList[i].centroid = clusterList[i].mean
+    for i in clusterList:  # calculate the mean and assign it as the new centroid
+        i.pre_mean = i.centroid
+        i.mean = mean(i, dataPoints)
+        i.centroid = i.mean
 
-    for i in range(clusterNumber):  # calculate the sum of change of centroids
-        change_root = np.linalg.norm(clusterList[i].pre_mean, clusterList[i].centroid)
+    for i in clusterList:  # calculate the sum of change of centroids
+        change_root = np.linalg.norm(i.pre_mean-i.centroid)
         change += change_root**2
+
+    for i in clusterList:  # clean up all the assigned point list, prepare for re-assign
+        i.pointStore = i.pointAssign
+        i.pointAssign = []
 
 
 # display the result
 print('1. The number of points: ' + str(pointNumber) + ';')
 print('   The number of dimensions: ' + str(dimension-1) + ';')
 print('   The value of k: ' + str(clusterNumber))
+print('------------------------------------------------------')
+
 print('2. The number of iterations: ' + str(iteration))
+print('------------------------------------------------------')
+
 print('3. Cluster data:')
 for i in range(clusterNumber):  # display each cluster
     clusterList[i].SSE = SSE(clusterList[i], dataPoints)
     SSE_sum += clusterList[i].SSE
     print('Cluster ' + str(i+1) + ' :')
     clusterList[i].display()
+
+print('------------------------------------------------------')
 print('4. The SSE score is :' + str(SSE_sum))
